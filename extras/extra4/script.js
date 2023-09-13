09aa8dfcbd6628051ec2c920275a9bcb6e5748bb
dominion-30 <abiolaayobamidele@gmail.com>
2023-09-13 15:54:05 +0100

    add file

---
+++ extras/extra4/script.js
@@ -0,0 +1,30 @@
+const sounds = ['applause', 'tada','victory', 'wrong', 'boo', 'gasp']
+
+sounds.forEach(sound=>{
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+
+    document.getElementById("buttons").appendChild(btn)
+
+    
+
+    btn.addEventListener("click", ()=>{
+
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+})
+
+function stopSongs(){
+    
+
+    sounds.forEach(sound =>{
+
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+
+    })
+}
