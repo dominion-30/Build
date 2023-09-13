c9adafee7c66350a7b198e6b00da8ed36eae6a58
dominion-30 <abiolaayobamidele@gmail.com>
2023-09-13 14:17:04 +0100

    add file

---
+++ project9/script.js
@@ -0,0 +1,30 @@
+
+const sounds = ['applause', 'boo', "wrong", 'victory', "tada", "gasp"];
+
+sounds.forEach(sound =>{
+    const btn = document.createElement("button")
+    btn.classList.add("btn")
+
+    btn.innerText = sound
+
+    btn.addEventListener(('click'), ()=>{
+        stopSongs()
+
+        document.getElementById(sound).play()
+
+    })
+
+    document.getElementById("buttons").appendChild(btn)
+})
+
+function stopSongs(){
+    
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    }
+
+    )
+}
