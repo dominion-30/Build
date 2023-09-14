86e8cf9f17b5f3f1e614b8cfb9d4eddcb391dd55
dominion-30 <abiolaayobamidele@gmail.com>
2023-09-14 12:36:35 +0100

    add file

--- project10/script.js
+++ project10/script.js
@@ -12,7 +12,8 @@ generateJoke()
 jokeBtn.addEventListener("click", generateJoke)
 
 
-function generateJoke(){
+//Using async/await
+async function generateJoke(){
 
     const config = {
         headers:{
@@ -21,11 +22,28 @@ function generateJoke(){
     }
 
 
-    fetch("https://icanhazdadjoke.com/", config)
-    .then((response) => response.json())
-    .then((answer) => {
-        jokeEl.innerHTML = answer.joke
-    })
+   const response = await fetch("https://icanhazdadjoke.com/", config)
+   const answer = await response.json()
+   jokeEl.innerHTML = answer.joke
+   
 }
+
+
+//Using .then()
+// function generateJoke(){
+
+//     const config = {
+//         headers:{
+//             "accept":"application/json"
+//         }
+//     }
+
+
+//     fetch("https://icanhazdadjoke.com/", config)
+//     .then((response) => response.json())
+//     .then((answer) => {
+//         jokeEl.innerHTML = answer.joke
+//     })
+// }
 
 
