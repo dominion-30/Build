d61e0665aabe2df382479cb47f19d544eb3a23de
dominion-30 <abiolaayobamidele@gmail.com>
2023-09-16 10:53:14 +0100

    add file

---
+++ extras/extra_/script.js
@@ -0,0 +1,23 @@
+// function showMessage(){
+//     alert("Hello everyone");
+// }
+
+// showMessage();
+
+
+// let add = (a,b) => a + b;
+
+// alert(add(1,5));
+
+
+let double = n => n * 2;
+alert(double(50));
+
+let Hi = ()=> "hello";
+
+alert(Hi());
+
+
+let query = (question,yes, no) => {
+
+}
